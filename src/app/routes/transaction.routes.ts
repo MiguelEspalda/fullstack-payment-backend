bfade1cb36429bef777c7c4f3466b3da0723b195
MiguelEspalda <martinezcobosm@gmail.com>
2025-04-10 01:49:12 -0500

    Ruta de Controlador de Updated Fix

--- src/app/routes/transaction.routes.ts
+++ src/app/routes/transaction.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { createTransaction, updatedTransaction, getAllTransactions } from '../controllers/transaction.controller'
+import { createTransaction, updateTransaction, getAllTransactions } from '../controllers/transaction.controller'
 
 export const transactionRoutes = Router()
 
 transactionRoutes.get('/', getAllTransactions)
 transactionRoutes.post('/', createTransaction)
-transactionRoutes.put('/:id', updatedTransaction)
+transactionRoutes.put('/:id', updateTransaction)
